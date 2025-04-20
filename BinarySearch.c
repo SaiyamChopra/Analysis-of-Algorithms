ce629258df2c907455c5b9ce2a5a90e08e70058d
SaiyamChopra <saiyamchopra447@gmail.com>
2025-04-20 15:14:47 +0530

    BinarySearch.c

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Binary search function
 int binarySearch(int arr[], int low, int high, int x)
 {
     while (low <= high) 
@@ -15,24 +16,57 @@ int binarySearch(int arr[], int low, int high, int x)
     return -1;
 }
 
+// Selection sort function
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++) 
+    {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) 
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        // Swap the found minimum with the first element
+        if (min_idx != i) 
+        {
+            int temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+        }
+    }
+}
+
 int main(void)
 {
-    int no;
+    int number;
     printf("Enter the size of list: ");
-    scanf("%d",&no);
-    int arr[no];
+    scanf("%d", &number);
+    int arr[number];
+
     printf("Enter the elements: ");
-    for (int x=0; x < no; x++)
+    for (int x = 0; x < number; x++)
     {
-        scanf("%d",&arr[x]);
+        scanf("%d", &arr[x]);
     }
-    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Sort the array before binary search
+    selectionSort(arr, number);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < number; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
     int x;
     printf("Enter the element to search: ");
     scanf("%d", &x);
-    int result = binarySearch(arr, 0, n - 1, x);
-   if(result == -1)
-        printf("Element is not present in array");
-   else
-        printf("Element is present at index %d",result);
+
+    int result = binarySearch(arr, 0, number - 1, x);
+    if (result == -1)
+        printf("Element is not present in array\n");
+    else
+        printf("Element is present at index %d\n", result);
+
+    return 0;
 }
